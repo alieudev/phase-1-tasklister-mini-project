83ae3eeffe93dc4cdb40a0b3a5f42ff5ba1c1e8c
Alieu Baldeh <alieu.dev@gmail.com>
2021-07-08 16:23:03 -0400

    task list completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // let form = document.querySelector('create-task-form')
+  //   
+  //   let task = event.elements.name.value 
+ 
+  // })
+  form()
+
 });
+
+function form() {
+  let form = document.querySelector('#create-task-form')
+  form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    let taskText = form.elements.ntask.value
+
+    let taskLi = document.createElement('li')
+    taskLi.textContent = taskText
+
+    let taskList = document.querySelector('#tasks')
+    taskList.append(taskLi)
+  })
+}
